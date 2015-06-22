8d2b1bbbaa6078384a7a877db458bbeb4e36edb6
Tobias Haar <tobias.haar@gmail.com>
2015-06-23 00:03:22 +0100

    use physfs for path lookups

---
+++ include/dioptre/filesystem/null/filesystem.h
@@ -0,0 +1,30 @@
+#ifndef DIOPTRE_FILESYSTEM_NULL_FILESYSTEM_H_
+#define DIOPTRE_FILESYSTEM_NULL_FILESYSTEM_H_
+
+#include <string>
+
+#include "dioptre/filesystem/filesystem_interface.h"
+
+namespace dioptre {
+namespace filesystem {
+namespace null {
+
+class Filesystem : public dioptre::filesystem::FilesystemInterface {
+public:
+  virtual ~Filesystem();
+
+  // Module
+  int initialize() { return 0; }
+  void destroy() { }
+
+  void registerFromArgs(char *argv[]) { }
+  void registerPath(std::string path) { }
+  void clear() { }
+  std::string find(std::string file) { return file; }
+}; // Filesystem
+
+} // null
+} // filesystem
+} // dioptre
+
+#endif // DIOPTRE_FILESYSTEM_NULL_FILESYSTEM_H_
---
+++ include/dioptre/filesystem/physfs/filesystem.h
@@ -0,0 +1,26 @@
+#ifndef DIOPTRE_FILESYSTEM_PHYSFS_FILESYSTEM_H_
+#define DIOPTRE_FILESYSTEM_PHYSFS_FILESYSTEM_H_
+
+#include "dioptre/filesystem/filesystem_interface.h"
+
+namespace dioptre {
+namespace filesystem {
+namespace physfs {
+
+class Filesystem : public dioptre::filesystem::FilesystemInterface {
+public:
+  // Module
+  int initialize() { return 0; }
+  void destroy();
+
+  void registerFromArgs(char *argv[]);
+  void registerPath(std::string path);
+  void clear();
+  std::string find(std::string file);
+}; // Filesystem
+
+} // physfs
+} // filesystem
+} // dioptre
+
+#endif // DIOPTRE_FILESYSTEM_PHYSFS_FILESYSTEM_H_
